d006fc85ece5ff2b3ea1921108810f5da4a04663
Furqon <75513502+furqon3009@users.noreply.github.com>
2026-07-15 21:29:07 +0930

    Update data_model_configs.py

--- configs/data_model_configs.py
+++ configs/data_model_configs.py
@@ -16,38 +16,10 @@ class EEG():
         self.class_names = ['W', 'N1', 'N2', 'N3', 'REM']
         self.sequence_len = 3000
         self.scenarios = [("0", "11"), ("12", "5"), ("7", "18"), ("16", "1"), ("9", "14")]
-        # self.src_domains = ['0', '7', '9', '12', '16']
-        self.src_domains = ['0', '7', '9']
-        # self.src_domains = ['0']
-        # self.src_domains = ['7']
-        # self.src_domains = ['9']
-        # self.src_domains = ['12']
-        # self.src_domains = ['16']
-        # self.src_domains = ['0', '7', '9']
-        self.trg_domains = ['1', '5', '11', '14', '18']
-        # self.trg_domains = ['5', '11', '14', '18']
-        # self.trg_domains = ['1']
-        # self.trg_domains = ['5']
-        # self.trg_domains = ['11']
-        # self.trg_domains = ['14']
-        # self.trg_domains = ['18']
-        # self.scenarios = [("9", "14")]
         self.shuffle = True
         self.drop_last = True #True
         self.normalize = True
         self.noise_rate = 0.2
-
-        # self.jitter_scale_ratio = 0.5 # 13 Nov
-        # self.jitter_ratio = 2
-        # self.max_seg = 12
-
-        # self.jitter_scale_ratio = 0.05 # 20 Nov
-        # self.jitter_ratio = 2.4
-        # self.max_seg = 12
-
-        # self.jitter_scale_ratio = 0.08 # 11 Maret
-        # self.jitter_ratio = 2.5
-        # self.max_seg = 12
 
         self.jitter_scale_ratio = 0.005
         self.jitter_ratio = 2.5
@@ -57,15 +29,9 @@ class EEG():
         self.input_channels = 1
         self.kernel_size = 25
         self.stride = 6
-        # self.dropout = 0.2
         self.dropout_src = 0.0000005
-        self.dropout = 0.0000005 # 4 Nov
-        # self.dropout = 0.005 
-        # self.dropout = 0.000005 # HugFace
-        # self.prompt_length = 40
+        self.dropout = 0.0000005 
         self.prompt_length = 5 
-        # self.prompt_length = 40 # 6 Nov
-        # self.prompt_length = 24
 
         # features
         self.mid_channels = 16
@@ -88,16 +54,8 @@ class FD():
     def __init__(self):
         super(FD, self).__init__()
         self.sequence_len = 5120
-        # self.scenarios = [("0", "1"), ("1", "2"), ("3", "1"), ("1", "0"), ("2", "3")]
-        self.scenarios = [("2", "3")]
+        self.scenarios = [("0", "1"), ("1", "2"), ("3", "1"), ("1", "0"), ("2", "3")]
         self.class_names = ['Healthy', 'D1', 'D2']
-        self.src_domains = ['0', '1', '2', '3']
-        # self.src_domains = ['0', '1']
-        self.trg_domains = ['0', '1', '2', '3']
-        # self.trg_domains = ['0']
-        # self.trg_domains = ['1']
-        # self.trg_domains = ['2']
-        # self.trg_domains = ['3']
         self.num_classes = 3
         self.shuffle = True
         self.drop_last = False
@@ -112,7 +70,6 @@ class FD():
         self.kernel_size = 32
         self.stride = 6
         self.dropout = 0.5
-        # self.dropout = 0.0000005 # 4 Nov
         self.dropout_src = 0.0000005
         self.prompt_length = 5 
 
@@ -145,22 +102,9 @@ class HAR():
     def __init__(self):
         super(HAR, self)
         self.scenarios = [("2", "11"), ("6", "23"), ("7", "13"), ("9", "18"), ("12", "16"),  ]
-        # self.scenarios = [("7", "13")]
-
         self.class_names = ['walk', 'upstairs', 'downstairs', 'sit', 'stand', 'lie']
-        self.src_domains = ['2', '6', '7', '9', '12']
-        # self.src_domains = ['2']
-        # self.src_domains = ['2', '6', '7']
-        # self.src_domains = ['9', '12']
-        self.trg_domains = ['11', '13', '16', '18', '23']
-        # self.trg_domains = ['11']
-        # self.trg_domains = ['13']
-        # self.trg_domains = ['16']
-        # self.trg_domains = ['18']
-        # self.trg_domains = ['23']
         self.sequence_len = 128
         self.shuffle = True
-        # self.drop_last = True #False
         self.drop_last = False
         self.normalize = True
 
@@ -181,14 +125,11 @@ class HAR():
         self.mid_channels = 64
         self.final_out_channels = 128
         self.features_len = 1
-        # self.features_len = 18 for sequential methods
         self.AR_hid_dim = 128
 
         # auto encoder
-        self.mid_dim = 375 #asli
-        self.out_dim = 250 #asli
-        # self.mid_dim = 256
-        # self.out_dim = 128
+        self.mid_dim = 375 
+        self.out_dim = 250 
         self.TSlength_aligned = 1024
 
 
